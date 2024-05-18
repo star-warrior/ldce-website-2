5fa2b080a65c06078c8d28cdb68d718d0a736fb6
Punja007 <kuchhadiyapunja007@gmail.com>
2024-05-18 09:38:15 +0530

    changed the counter animation text, symbols, background and added on scroll effect to it by punja

--- js/main.js
+++ js/main.js
@@ -31,10 +31,22 @@
 
 
     // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 5,
-        time: 2000
+    $(document).ready(function() {
+        // Initialize CounterUp on elements with data-toggle="counter-up"
+        $('[data-toggle="counter-up"]').counterUp({
+            delay: 5,
+            time: 2000
+        });
+
+        // Attach CounterUp to the scroll event using Waypoints
+        $('[data-toggle="counter-up"]').waypoint(function() {
+            $(this.element).counterUp({
+                delay: 5,
+                time: 2000
+            });
+        }, { offset: 'bottom-in-view' });
     });
+
 
 
     // Event carousel
